cff0d78c4b96b0c2a224504069194b0d02ec384c
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-08-29 02:46:56 +0530

    Update index.js

--- backend_/index.js
+++ backend_/index.js
@@ -91,7 +91,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:3001', 
   credentials: true,
 }));
 
