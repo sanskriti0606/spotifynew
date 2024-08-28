a9dad05bfa1c6b36a594689911d9c2dc865f53c1
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-08-29 02:32:53 +0530

    Update index.js

--- backend_/index.js
+++ backend_/index.js
@@ -90,7 +90,10 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true,
+}));
 
 app.use("/api/users", userRouter);
 app.use("/api/music", songRouter);
