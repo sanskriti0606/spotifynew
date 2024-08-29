3c8dcff1ef1217d673979f23a8f77c9bb2051791
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-08-30 02:51:59 +0530

    Update album.js

--- backend_/src/models/album.js
+++ backend_/src/models/album.js
@@ -1,34 +1,34 @@
-// src/models/Album.js
+// // src/models/Album.js
 
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const albumSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    artist: {
-        type: String,
-        required: true,
-    },
-    releaseDate: {
-        type: Date,
-        required: true,
-    },
-    genre: {
-        type: String,
-        required: true,
-    },
-    songs: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Song', // Reference to the Song model
-        }
-    ],
-}, {
-    timestamps: true, // Adds createdAt and updatedAt fields
-});
+// const albumSchema = new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     artist: {
+//         type: String,
+//         required: true,
+//     },
+//     releaseDate: {
+//         type: Date,
+//         required: true,
+//     },
+//     genre: {
+//         type: String,
+//         required: true,
+//     },
+//     songs: [
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: 'Song', // Reference to the Song model
+//         }
+//     ],
+// }, {
+//     timestamps: true, // Adds createdAt and updatedAt fields
+// });
 
-const Album = mongoose.model('Album', albumSchema);
+// const Album = mongoose.model('Album', albumSchema);
 
-export default Album;
+// export default Album;
